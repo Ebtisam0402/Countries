0ed0423b3252ea7eb5700dae1b6e3dd6601a6d27
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2026-05-15 09:51:58 -0700

    user interaction added

--- src/componentes/Main.jsx
+++ src/componentes/Main.jsx
@@ -5,13 +5,42 @@ export default function Main() {
     const [countries, setCountries] = useState([])
     const [loading, setLoading ] = useState(true)
     const [error, setError] = useState("")
+    const [searchTerm, setSearchTerm]= useState("")
 
     useEffect (() => {
 
         fetch("https://restcountries.com/v3.1/all?fields=name,capital,currencies")
         .then(res => {
+                if (!res.ok) {
+                    throw Error("Failed to fetch countries")
+                }
+
+                return res.json()
+            })
+            .then(data => {
+                setCountries(data)
+                setLoading(false)
+            })
+            .catch(err => {
+                setError(err.message)
+                setLoading(false)
+            })
+       
+    }, [])
+
+   
+    function searchCountry(event) {
+        event.preventDefault()
+
+         setLoading(true)
+         setError("")
+
+        fetch(
+            `https://restcountries.com/v3.1/name/${searchTerm}?fields=name,capital,currencies`
+        )
+             .then(res => {
             if(!res.ok) {
-                throw Error ("Failed to fetch countries")
+                throw Error ("Country not found")
             }
             return res.json()
         })
@@ -23,9 +52,10 @@ export default function Main() {
             setError(err.message)
             setLoading(false)
         })
-    }, [])
 
-    if(loading) {
+        
+    }
+     if(loading) {
         return <h2>Loading countries</h2>
     }
 
@@ -35,6 +65,21 @@ export default function Main() {
 
     return (
         <main>
+
+         <form onSubmit={searchCountry}>
+
+                <input
+                    type="text"
+                    placeholder="Search country"
+                    value={searchTerm}
+                    onChange={(event) =>
+                        setSearchTerm(event.target.value)
+                    }
+                />
+
+                <button>Search</button>
+
+            </form>
           {countries.slice(0, 10).map(country => (
                 <CountryItem 
                     key={country.name.common}
